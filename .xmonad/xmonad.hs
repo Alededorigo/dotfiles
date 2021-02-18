9c6fa3c8e8e1d4372ae1377d1e84547b6a2584ae
alededorigo <alededorigo@gmail.com>
2021-02-18 08:10:51 +0100

    Improved comments

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -45,84 +45,48 @@ myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
 
     -- Volume keys
     , ((0                     , 0x1008FF11), spawn "~/.scripts/volume.sh -d")
-
     , ((0                     , 0x1008FF13), spawn "~/.scripts/volume.sh -u")
-
     , ((0                     , 0x1008FF12), spawn "amixer set Master toggle")
 
     -- Music keys
     , ((0, xF86XK_AudioPlay), spawn "mocp --toggle-pause")
-
     , ((0, xF86XK_AudioPlay), spawn "mocp --toggle-pause")
-
     , ((0, xF86XK_AudioNext), spawn "mocp --next")
-
     , ((0, xF86XK_AudioPrev), spawn "mocp --previous")
 
     -- Brightness keys
 
     , ((0,                    xF86XK_MonBrightnessUp), spawn "brightnessctl s +5%")
-
     , ((0,                    xF86XK_MonBrightnessDown), spawn "brightnessctl s 5-%")
 
-    -- Rofi with Dmenu theme
+    -- My keybindings
+
     , ((modm,               xK_space     ), spawn "~/.scripts/rofilaunch.sh")
-
-    -- Panel
     , ((modm,               xK_q     ), spawn "bash ~/.scripts/panel")
-
-    -- Flameshot (for screenshots)
     , ((modm,               xK_x     ), spawn "flameshot gui")
-
-    -- Poweroff (for Systemd)
     , ((modm .|. shiftMask, xK_p     ), spawn "rofi -show p -modi p:rofi-power-menu -width 20 -lines 5")
 
-    -- Kill
+    -- Edited
+
     , ((modm,               xK_c     ), kill)
-
-    -- Change layout algorithm
     , ((modm,               xK_t ), sendMessage NextLayout)
-
-    --  Reset the layouts on the current workspace to default
-    , ((modm .|. shiftMask, xK_space ), setLayout $ XMonad.layoutHook conf)
-
-    -- Resize viewed windows to the correct size
-    , ((modm,               xK_n     ), refresh)
-
-    -- Move focus to the next window
-    , ((modm,               xK_Tab   ), windows W.focusDown)
-
-    -- Move focus to the next window
-    , ((modm,               xK_j     ), windows W.focusDown)
-
-    -- Move focus to the previous window
-    , ((modm,               xK_k     ), windows W.focusUp  )
-
-    -- Move focus to the master window
-    , ((modm,               xK_m     ), windows W.focusMaster  )
-
-    -- Swap the focused window and the master window
-    , ((modm .|. shiftMask, xK_Return), windows W.swapMaster)
-
-    -- Swap the focused window with the next window
-    , ((modm .|. shiftMask, xK_j     ), windows W.swapDown  )
-
-    -- Swap the focused window with the previous window
-    , ((modm .|. shiftMask, xK_k     ), windows W.swapUp    )
-
-    -- Shrink the master area
-    , ((modm,               xK_h     ), sendMessage Shrink)
-
-    -- Expand the master area
-    , ((modm,               xK_l     ), sendMessage Expand)
-
-    -- Put windows from floating to tiling
     , ((modm,               xK_f     ), withFocused $ windows . W.sink)
+    , ((modm .|. shiftMask, xK_x     ), io (exitWith ExitSuccess))
+    , ((modm .|. shiftMask, xK_g     ), spawn "xmonad --recompile; xmonad --restart")
 
-    -- Increment the number of windows in the master area
+    -- Default
+    , ((modm .|. shiftMask, xK_space ), setLayout $ XMonad.layoutHook conf)
+    , ((modm,               xK_n     ), refresh)
+    , ((modm,               xK_Tab   ), windows W.focusDown)
+    , ((modm,               xK_j     ), windows W.focusDown)
+    , ((modm,               xK_k     ), windows W.focusUp  )
+    , ((modm,               xK_m     ), windows W.focusMaster  )
+    , ((modm .|. shiftMask, xK_Return), windows W.swapMaster)
+    , ((modm .|. shiftMask, xK_j     ), windows W.swapDown  )
+    , ((modm .|. shiftMask, xK_k     ), windows W.swapUp    )
+    , ((modm,               xK_h     ), sendMessage Shrink)
+    , ((modm,               xK_l     ), sendMessage Expand)
     , ((modm              , xK_comma ), sendMessage (IncMasterN 1))
-
-    -- Deincrement the number of windows in the master area
     , ((modm              , xK_period), sendMessage (IncMasterN (-1)))
 
     -- Toggle the status bar gap
@@ -130,13 +94,6 @@ myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
     -- See also the statusBar function from Hooks.DynamicLog.
     --
     -- , ((modm              , xK_b     ), sendMessage ToggleStruts)
-
-    -- Logout
-    , ((modm .|. shiftMask, xK_x     ), io (exitWith ExitSuccess))
-
-    -- Reload configuration
-    , ((modm .|. shiftMask, xK_g     ), spawn "xmonad --recompile; xmonad --restart")
-
     -- Run xmessage with a summary of the default keybindings (useful for beginners)
     , ((modm .|. shiftMask, xK_slash ), spawn ("echo \"" ++ help ++ "\" | xmessage -file -"))
     ]
@@ -179,17 +136,9 @@ myMouseBindings (XConfig {XMonad.modMask = modm}) = M.fromList $
 
 --------------  LAYOUTS BINDINGS  ---------------
 
--- You can specify and transform your layouts by modifying these values.
--- If you change layout bindings be sure to use 'mod-shift-space' after
--- restarting (with 'mod-q') to reset your layout state to the new
--- defaults, as xmonad preserves your old layout settings by default.
---
--- The available layouts.  Note that each layout is separated by |||,
--- which denotes layout choice.
---
 myLayout = tiled ||| Mirror tiled ||| Full
   where
-     -- default tiling algorithm partitions the screen into two panes
+     -- Default (with gaps)
      tiled = spacing 13 $ Tall nmaster delta ratio
 
      -- The default number of windows in the master pane
@@ -203,18 +152,7 @@ myLayout = tiled ||| Mirror tiled ||| Full
 
 --------------  WINDOW RULES  ---------------
 
--- Execute arbitrary actions and WindowSet manipulations when managing
--- a new window. You can use this to, for example, always float a
--- particular program, or have a client always appear on a particular
--- workspace.
---
--- To find the property name associated with a program, use
--- > xprop | grep WM_CLASS
--- and click on the client you're interested in.
---
--- To match on the WM_NAME, you can use 'title' in the same way that
--- 'className' and 'resource' are used below.
---
+-- To get the class type "xprop | grep CLASS"
 myManageHook = composeAll
     [ className =? "MPlayer"        --> doFloat
     , className =? "Gimp"           --> doFloat
