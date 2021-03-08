2c39e76a47d5c5e56dcccf7fe3b59ee7dff18a36
Alededorigo <alededorigo@protonmail.com>
2021-03-08 18:58:46 +0100

    Improved screenshot script: new theme, new icon and notifications.

--- .config/rofi/scripts/screenshot.sh
+++ .config/rofi/scripts/screenshot.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-rofi_command="rofi -lines 3"
+rofi_command="rofi -lines 3 -theme scr"
 
 # Options
-screen=" Full screen"
-area="鉶Select area"
-timer=" Wait 5 seconds"
+screen=" "
+area="麗"
+timer=" "
 
 # Variable passed to rofi
 options="$screen\n$area\n$timer"
@@ -13,12 +13,15 @@ options="$screen\n$area\n$timer"
 chosen="$(echo -e "$options" | $rofi_command -dmenu -p "shot" -selected-row 1)"
 case $chosen in
     $screen)    
-        maim ~/Pics/$(date +%d_%m_%y).png
+        maim ~/Pics/Shots/$(date +%d-%m-%y).png
+        notify-send -i ~/.icons/noti/wall.png "Screenshot" "Saved to ~/Pics/Shots/$(date +%d-%m-%y).png"
     ;;
     $area)      
         maim -sqo --nodecorations=1 --color=186,137,145,255 | xclip -selection clipboard -t image/png
+        notify-send -i ~/.icons/noti/wall.png "Screenshot" "Copied to clipboard"
     ;;
     $timer)
         sleep 5 && maim -sqo --nodecorations=1 --color=186,137,145,255 | xclip -selection clipboard -t image/png
+        notify-send -i ~/.icons/noti/wall.png "Screenshot" "Copied to clipboard"
     ;;
 esac
