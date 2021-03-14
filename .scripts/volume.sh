c96561e80d79deaf1a8a1e747e0cdf385b57c5de
alededorigo <alededorigo@protonmail.com>
2021-03-14 13:06:15 +0100

    Minor edits

--- .scripts/volume.sh
+++ .scripts/volume.sh
@@ -4,10 +4,10 @@ if [[ "$1" == "-u" ]]; then
     killall dunst
     dunst &
     amixer set Master 5%+
-    notify-send -i ~/.icons/noti/volu.png "$(awk -F"[][]" '/dB/ { print $2 }' <(amixer sget Master))"
+    notify-send -i ~/.icons/noti/volu.png "$(awk -F"[][]" '/Left:/ { print $2 }' <(amixer sget Master))"
 elif [[ "$1" == "-d" ]]; then
     killall dunst
     dunst &
     amixer set Master 5%-
-    notify-send -i ~/.icons/noti/vold.png "$(awk -F"[][]" '/dB/ { print $2 }' <(amixer sget Master))"
+    notify-send -i ~/.icons/noti/vold.png "$(awk -F"[][]" '/Left:/ { print $2 }' <(amixer sget Master))"
 fi
