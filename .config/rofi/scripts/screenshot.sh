fb59e702528417ba869202781c1f96a2ada936ac
alededorigo <alededorigo@protonmail.com>
2021-03-18 12:49:08 +0100

    Minor edits

--- .config/rofi/scripts/screenshot.sh
+++ .config/rofi/scripts/screenshot.sh
@@ -13,11 +13,9 @@ options="$screen\n$area\n$timer"
 chosen="$(echo -e "$options" | $rofi_command -dmenu -p "shot" -selected-row 1)"
 case $chosen in
     $screen)
-        maim ~/Pics/Shots/$(date +%d-%m-%y-%H:%M).png
-        notify-send -i ~/.icons/noti/wall.png "Screenshot" "Saved to ~/Pics/Shots/$(date +%d-%m-%y_%H:%M).png"
-        sxiv ~/Pics/Shots/$(date +%d-%m-%y-%H:%M).png
+        maim ~/Pics/Shots/$(date +%d-%m-%y-%H:%M).png && notify-send -i ~/.icons/noti/wall.png "Screenshot" "Saved to ~/Pics/Shots/$(date +%d-%m-%y_%H:%M).png" && sxiv ~/Pics/Shots/$(date +%d-%m-%y-%H:%M).png
     ;;
-    $area)      
+    $area)
         maim -sqo --nodecorations=1 --color=186,137,145,255 | xclip -selection clipboard -t image/png
         notify-send -i ~/.icons/noti/wall.png "Screenshot" "Copied to clipboard"
     ;;
